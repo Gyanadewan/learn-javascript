3109d6e86a3c85fc5dd4f8a41af8aebb4a394b30
Gyanadewan <gyanadewan1@gmail.com>
2026-07-15 23:59:34 +0600

    added type of check

---
+++ type.js
@@ -0,0 +1,38 @@
+//  number
+const mangoes = 3;
+console.log(typeof mangoes)
+//  string
+const fruit = "hello"
+console.log(typeof fruit)
+
+// boolean
+const student = true;
+console.log(typeof student)
+
+// undifined
+let age;
+console.log(typeof age)
+
+// object 
+const person = {
+    name: "Gyana",
+    age: 18
+}
+console.log( typeof person)
+
+// Array but output obj
+const numbers = [1,2,3,];
+console.log(typeof numbers)
+
+// Array check 
+const numbers2 = [1, 2, 3];
+console.log(Array.isArray(numbers)); 
+
+// function
+function greet (){
+console.log("Hello")
+}
+console.log(typeof greet)
+//  null
+const data = null;
+console.log(typeof data); // "object"
