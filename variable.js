f339bcfd5a24396532a01f0e9ff2b9f140d8b01f
Gyanadewan <gyanadewan1@gmail.com>
2026-07-15 21:48:56 +0600

    learn var, let, and cons

---
+++ variable.js
@@ -0,0 +1,20 @@
+var weight = 38;
+var temperature = 38;
+var people = 38;
+
+console.log(temperature)
+
+// lets, const
+
+// lets er value change kora jai
+let price = 100;
+price = 50 ;
+
+console.log (price)
+
+// const value change kora jai nah
+const pI =  3.14;
+
+// PI = 30;
+// console.log(pI)
+
