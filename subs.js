99e46bff11dfe21c8253e167dd2f761f5ba7be12
Gyanadewan <gyanadewan1@gmail.com>
2026-07-16 14:09:42 +0600

    practice subtraction

---
+++ subs.js
@@ -0,0 +1,8 @@
+const firstNumber = 50;
+const secondNumber  = 30;
+// console.log(firstNumber-secondNumber)
+
+const firsNumber1 =10;
+const seconNumber2 = 5;
+const subtraction = (firsNumber1-seconNumber2)
+console.log(subtraction)
