8a8528b5f5f704406b81327067fc60138fd7aa63
Gyanadewan <gyanadewan1@gmail.com>
2026-07-16 14:04:34 +0600

    addition and toFixed in JavaScript

--- add.js
+++ add.js
@@ -6,3 +6,17 @@ const number2 = 40;
 
 const sum = number1 + number2;
 console.log(sum)
+
+
+const firstNumber = 0.1;
+const secondNumber = 0.2;
+
+const total = firstNumber + secondNumber;
+// console.log(total)  output: 0.30000000000000004
+const totalTwoDecimal = total.toFixed(2); 
+// console.log(totalTwoDecimal) output: 0.30
+
+const totalTwoDecimalNumber = parseFloat(totalTwoDecimal)
+console.log(totalTwoDecimalNumber)
+
+
