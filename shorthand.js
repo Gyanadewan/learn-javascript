7368429039f2f51b0be87cb92cd118cfecc6aee6
Gyanadewan <gyanadewan1@gmail.com>
2026-07-16 14:45:39 +0600

    pracitice  shorthan operators

---
+++ shorthand.js
@@ -0,0 +1,21 @@
+let price = 100;
+
+// Addition
+price += 20;
+console.log(price); // 120
+
+// Subtraction
+price -= 10;
+console.log(price); // 110
+
+// Multiplication
+price *= 2;
+console.log(price); // 220
+
+// Division
+price /= 11;
+console.log(price); // 20
+
+// Modulo
+price %= 3;
+console.log(price); // 2
