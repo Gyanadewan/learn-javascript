0e68cf920fdb4696f88c769448f880d894768afe
Gyanadewan <gyanadewan1@gmail.com>
2026-07-18 22:29:39 +0600

    practice for loop

---
+++ forloop.js
@@ -0,0 +1,33 @@
+
+for (let i=0; i<= 10; i++){
+    console.log("value of i ",i)
+}
+
+// 1-20 purjanta even number check
+for (let i =1; i<=20; i++ ){
+   if (i % 2===0){
+    console.log(i)
+   }
+}
+// 1-20 purjanta even number check
+for (let i =1; i<=20; i++ ){
+   if (i % 2===0){
+    console.log(i)
+   }
+}
+// 1-20 purjanta odd number check
+for (let i=1; i<=20; i++ ){
+   if (i % 2==1){
+    console.log(i)
+   }
+}
+
+
+//  for loop use kore sum 
+
+let sum = 0;
+for (let i = 1; i<=10; i++){
+   sum = sum + i;
+}
+
+console.log(sum)
