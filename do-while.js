322e94bceef265b029bd07438722b41a4fb375b7
Gyanadewan <gyanadewan1@gmail.com>
2026-07-19 20:36:00 +0600

    practice do while loop

---
+++ do-while.js
@@ -0,0 +1,19 @@
+let count = 1;
+
+do{
+    console.log("count number is",count)
+    count++
+} while(count<=5)
+    
+
+//  sum of 1-10;
+
+let i = 1;
+let sum = 0;
+
+do{
+    sum += i;
+    i++;
+
+} while (i <=10);
+console.log(sum)
