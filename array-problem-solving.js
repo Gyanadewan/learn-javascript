f05b2f17db7b69d11deb986095b70e359d76830c
Gyanadewan <gyanadewan1@gmail.com>
2026-07-20 11:01:57 +0600

    JavaScript array beginner problem solving

---
+++ array-problem-solving.js
@@ -0,0 +1,63 @@
+// const fruits = ["jackfruit", "mango","apple"];
+// console.log("fruits ache" ,fruits)
+
+// const names = ["messi","ronaldo","m bappe"];
+// console.log(names.length)
+
+
+// const names = ["messi","ronaldo","m bappe"];
+// console.log(names[0])
+// console.log(names[2])
+
+// const names = ["messi","ronaldo","m bappe"];
+// names[1] = "haland"
+// console.log(names)
+
+
+// const names = ["messi","ronaldo","m bappe"];
+// names.push("haland")
+// console.log(names)
+
+
+// const names = ["messi","ronaldo","m bappe"];
+//  const result = names.pop()
+//  console.log(result)
+//  console.log(names)
+
+// const alphabat = ["b","c","d","e","f","g"]
+// alphabat.unshift("a")
+// console.log(alphabat)
+
+
+// const alphabat = ["a","b","c","d","e","f","g"]
+// const result = alphabat.shift()
+
+// console.log(result)
+// console.log(alphabat)
+
+// const names= ["jamal","kamal","rahul","bappa raj"]
+// console.log(names.includes("bappa raj"))
+// console.log(names.includes("bappa muzumder"))
+
+
+// const names= ["jamal","kamal","rahul","bappa raj"]
+// console.log(names.indexOf("bappa raj"))
+// console.log(names.indexOf("bappa muzumder"))
+
+
+// const names= ["jamal","kamal","rahul","bappa raj"]
+// for (let i =0; i<names.length; i++){
+//     console.log(names[i])
+// }
+
+// const names = ["jamal","kamal","rahul","bappa"];
+// for (let i=0 ; i<names.length; i++){
+//     console.log(names[i])
+// }
+
+const names = ["jamal","kamal","rahul","bappa"];
+let count = 0;
+while (count<names.length ){
+    console.log(names[count])
+    count++
+}
