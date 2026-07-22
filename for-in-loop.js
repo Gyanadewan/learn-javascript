6ddef568bc4e3930a622478f68e1603b8647ef46
Gyanadewan <gyanadewan1@gmail.com>
2026-07-22 21:10:55 +0600

    practice javascript loop (for of,for in )

---
+++ for-in-loop.js
@@ -0,0 +1,9 @@
+const person = { 
+     name: "Gyana",
+     age: 20, 
+     country: "Bangladesh"
+     };
+
+     for(let key in person){
+        console.log(key)
+     }
---
+++ for-of-loop.js
@@ -0,0 +1,4 @@
+const fruits = ["mango","banana","jackfruit","Lichi"]
+for ( fruit of fruits){
+    console.log(fruit)
+}
