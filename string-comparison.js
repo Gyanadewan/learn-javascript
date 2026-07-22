3fc58b02fb4fcf9aace7f841058891d0755b0189
Gyanadewan <gyanadewan1@gmail.com>
2026-07-22 23:41:13 +0600

    string comparison lowercase and uppercase

---
+++ string-comparison.js
@@ -0,0 +1,19 @@
+// const subject = "chemistry";
+// const book = "Chemistry";
+
+// if (subject===book.toLowerCase){
+//     console.log("Poira poira fatai felbo");
+// }
+// else{
+//     console.log("aibar ki auto pass ase!!!")
+// }
+
+
+const emailUserProvided = "goni@Mia.com";
+const emailSavedInDatabase = "goni@MIA.com";
+
+if (emailUserProvided.toUpperCase() === emailSavedInDatabase.toUpperCase()) {
+    console.log("Email matched");
+} else {
+    console.log("Email not matched");
+}
