50f3085ad18a5f9522ff7a5cfa0972eea191721d
Gyanadewan <gyanadewan1@gmail.com>
2026-07-23 10:09:25 +0600

    practice javascript string method trim()

---
+++ string-method-trim.js
@@ -0,0 +1,27 @@
+
+// trim()
+// ---------
+// 1. String Method
+// 2. শুরু ও শেষের space remove করে
+// 3. Return করে String
+
+// Syntax:
+// text.trim()
+
+// const  text = "  Hello  "
+// const result = text.trim();
+// console.log(result)
+
+
+
+// const text2 = " Hello boss "
+// console.log("|" + text2 +"|");
+
+// console.log("|"+ text2.trim() + "|");
+
+const text3 = " welcome boss "
+const result = text3.trim()
+console.log (result[0]) 
+console.log (result[1]) 
+console.log(result[9])
+
