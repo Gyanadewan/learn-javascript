f5801374fd5eee373f77b4094562f6d3c40f646a
Gyanadewan <gyanadewan1@gmail.com>
2026-07-26 23:21:00 +0600

    practice use a function array first element double

--- function-practice.js
+++ function-practice.js
@@ -10,4 +10,23 @@ function cutHalf (num){
 
  const result = cutHalf(6)
  console.log("result hobe",result)
- 
+
+
+ function DewanBabu (name){
+    const fullName = (name + " DewanBabu")
+    return fullName
+ }
+
+ const restultFullName =  DewanBabu("Gyana");
+ console.log(restultFullName)
+
+
+ function firstElementDouble (array){
+    // console.log("full array", array)
+    const firstElment = array[0];
+    return (firstElment * 2)
+ }
+
+ const numbers = [24,34,53,54,34,56];
+ const dobouledNumber = firstElementDouble(numbers)
+ console.log(dobouledNumber)
