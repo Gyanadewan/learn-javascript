27982dd6faada466da23148d2b5b2d23c9a27be1
Gyanadewan <gyanadewan1@gmail.com>
2026-07-31 23:03:16 +0600

    solve problem countAdults

---
+++ countAdults-problem-solving.js
@@ -0,0 +1,17 @@
+function countAdults (People){
+    if (!Array.isArray(People) || People.length === 0){
+       return "Invalid"
+    }
+    let adultCount = 0;
+   for ( let singleMan of People)
+     if ( singleMan.age >=18){
+        adultCount ++
+     }
+     return adultCount
+}
+
+ console.log (countAdults ([
+  { name: "Rahim", age: 17 },
+  { name: "Karim", age: 20 },
+  { name: "Jamal", age: 25 }
+]))
